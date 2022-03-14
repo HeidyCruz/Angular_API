e035141b40e79737f6b3715e670e3101fe4f9365
HeidyCruz <heidycruzher@gmail.com>
2022-03-13 19:03:45 -0600

    Http Get

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -5,6 +5,7 @@ import { Product } from '../../models/product.model';
 import { StoreService } from '../../services/store.service';
 import { ProductsService } from '../../services/products.service';
 
+
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
@@ -15,6 +16,7 @@ export class ProductsComponent implements OnInit {
   myShoppingCart: Product[] = [];
   total = 0;
   products: Product[] = [];
+  showProductDetail = false;
 
   constructor(
     private storeService: StoreService,
@@ -25,6 +27,7 @@ export class ProductsComponent implements OnInit {
 
   ngOnInit(): void {
     this.productsService.getAllProducts()
+    /**subscribe obtiene toda la informacion */
     .subscribe(data => {
       this.products = data;
     });
@@ -33,6 +36,17 @@ export class ProductsComponent implements OnInit {
   onAddToShoppingCart(product: Product) {
     this.storeService.addProduct(product);
     this.total = this.storeService.getTotal();
+  }
+
+  toggleProductDetail() {
+    this.showProductDetail = !this.showProductDetail;
+  }
+
+  onShowDetail(id: string) {
+    this.productsService.getProduct(id)
+    .subscribe(data => {
+      console.log('product', data);
+    })
   }
 
 }
